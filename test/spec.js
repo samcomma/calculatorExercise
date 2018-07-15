b621337d9d429d6cb0e138bc62f0fd4daccda425
samcomma <samcomerford1825@gmail.com>
2018-07-15 09:31:48 -0400

    added further testing

--- test/spec.js
+++ test/spec.js
@@ -7,6 +7,9 @@ describe('add', ()=> {
 	it('should add 1 and 2', ()=> {
 		expect(add(1,2)).to.eql(3)
 	})
+	it('should add negative numbers', ()=> {
+		expect(add(-4,-5)).to.eql(-9)
+	})
 });
 
 
@@ -14,10 +17,16 @@ describe('subtract', ()=> {
 	it('should add 1 and 2', ()=> {
 		expect(subtract(3,2)).to.eql(1)
 	})
+	it('should subtract negative numbers', ()=> {
+		expect(subtract(-3,3)).to.eql(-6)
+	})
 });
 
 describe('multiply', ()=> {
 	it('should multiply 4 and 5', ()=> {
 		expect(multiply(4,5)).to.eql(20)
+	})
+	it('should multiply negative numbers', ()=> {
+		expect(multiply(-8,-2)).to.eql(16)
 	})
 });
